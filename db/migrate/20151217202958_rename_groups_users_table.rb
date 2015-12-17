553f027ff2a97fa5d258009d2383d08fffe8cff0
Sue Li <zhl059@mail.harvard.edu>
2015-12-17 21:32:24 +0100

    renamed to group_users

---
+++ db/migrate/20151217202958_rename_groups_users_table.rb
@@ -0,0 +1,5 @@
+class RenameGroupsUsersTable < ActiveRecord::Migration
+  def change
+    rename_table :groups_users, :group_users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215190724) do
+ActiveRecord::Schema.define(version: 20151217202958) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
